70bc917aa571328a9e2ef0d616f75580ba2d917b
Kyle <reimers95@gmail.com>
2024-04-29 17:13:33 -0500

    new comment and print w/ arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
-    println!("Hello, world!");
-}
+    println!("first print in rust");
+    println!("Hello {0}, this is {1}, {1} this is {0}.","Alice","Bob")
+}/*adding block comment */
