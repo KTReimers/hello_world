0147349b89cee9b0274004191fca2017458311cc
Kyle <reimers95@gmail.com>
2024-04-29 21:03:14 -0500

    started noting primitive data types

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
 fn main() {
+    // print and formatting print statements
     println!("first print in rust");
-    println!("Hello {0}, this is {1}, {1} this is {0}.","Alice","Bob")
+    print!("Hello {0}, this is {1}, {1} this is {0}.","Alice","Bob"); //print! prints on one line while println! will add a new line
+
+// error print
+    eprint!("error in your code");
+
+    // primitive data types
+    // boolean
+    let logicalL: bool = true;
+    //float
+    let float_a: f64=1.0; //memory alotment
+    let float_b = 3.2; //defaults to f64
+    // integer
+    let int =5i32; //this is memory alotment
+    let int_a = 6; //defaults to i32
+    println!("{}",int); //can't just print the variable, you have to format it into a string
+    //data type can be inferred from context
+    let number = 45;
+
+    number = "hello"; //should error saying you cannot change type i.e. "expected integer, found `&str`"
+    println!("{}",number)
+
+
 }/*adding block comment */
